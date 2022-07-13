243a061db95151de392d54cb37a8bc48f5e57938
Tessa Collins <tessacollins525@gmail.com>
2022-07-13 14:05:15 +0200

    attempt at units

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from scipy.integrate import quad
 from scipy.optimize import curve_fit
 from scipy.misc import derivative
 from sympy import *
+from astropy import units as u
+from astropy import constants as c
+from astropy.coordinates import Angle
 
 
 # plt.style.use('ggplot')
@@ -37,19 +40,19 @@ def read_dat(filename):
 example_data = read_dat("Rotmass\Rotmass\IC4202_rotmass.dat")
 example_df = example_data["frame"]
 
-rad = example_df["Rad"].values #Cylindrical radius
+rad = example_df["Rad"].values*u.kpc #Cylindrical radius
+
 
 # ---------------- Rho_bulge ---------------------# 
 
 
-sdbul = example_df["SDbul"].values #Surface density
+sdbul = example_df["SDbul"].values*c.L_sun / (u.pc)**2 #Surface density
 
 
 
 def rho_bulge(z, R, rho_0, r_b):
-    r = (R**2 + z**2)**0.5
-    return rho_0 * np.exp(-r/r_b)
-    
+    r = (R**2 + z**2)**0.5 
+    return rho_0 * np.exp(-r/r_b)    
 
 def surface_density_buldge(Rs, rho_0, r_b):
     if hasattr(Rs, '__len__'):
@@ -61,7 +64,7 @@ def surface_density_buldge(Rs, rho_0, r_b):
 popt, pcov = curve_fit(surface_density_buldge, rad, sdbul) 
 
 def sdbul_interpolated(R):
-    return surface_density_buldge(R, popt[0], popt[1])
+    return surface_density_buldge(R, popt[0]*c.L_sun/ (u.pc)**2, popt[1]*u.kpc)
 
 
 # Differentiate surface density wrt rbar
@@ -77,19 +80,19 @@ def rho_buldge_final(R):
     return -1/np.pi * quad(func, 0, np.inf)[0]
 
 
-# # Checking the fit
-# inter = sdbul_interpolated(rad)
-# prime = sdbul_prime(rad)
+# Checking the fit
+inter = sdbul_interpolated(rad)
+prime = sdbul_prime(rad)
 
-# p = sns.scatterplot(x=rad, y=sdbul)
-# p.set_xlabel("Radius")
-# p.set_ylabel("Surface Density")
-# sns.lineplot(x=rad, y=inter)
-# sns.lineplot(x=rad, y=prime)
-# plt.show()
+p = sns.scatterplot(x=rad, y=sdbul)
+p.set_xlabel("Radius")
+p.set_ylabel("Surface Density")
+sns.lineplot(x=rad, y=inter)
+sns.lineplot(x=rad, y=prime)
+plt.show()
 
 # ---------------------- Rho_gas ----------------------- #
-sdgas = example_df["SDgas"].values 
+sdgas = example_df["SDgas"].values *c.L_sun / (u.pc)**2
 
 def surface_density_gas(R, sigma0, c1, c2, c3, c4, R_sig):
     return sigma0*(1 + c1*R + c2*R**2 + c3*R**3 + c4*R**4) * np.exp(-R/R_sig)
@@ -109,15 +112,17 @@ def sdgas_interpolated(R):
 # plt.show()
 
 def rho_gas(R):
-    return 1.4 * sdgas_interpolated(R)/ (np.sqrt(2*np.pi)* 0.130e3) # 1.4 to account for lack of H2 data. 0.130e3 is 0.130 kpc from Hossenfelder et al
+    return 1.4 * sdgas_interpolated(R)/ (np.sqrt(2*np.pi)* 0.130*u.kpc) # 1.4 to account for lack of H2 data, 0.130 kpc from Hossenfelder et al
 
 #--------------------Rho_disk (*)---------------------------#
 
-sddisk = example_df["SDdisk"]
+sddisk = example_df["SDdisk"].values *c.L_sun / (u.pc)**2
 
 def rho_disk(z, R, rho_0, Rstar):
-    zstar = 0.196*Rstar**0.633 #kpc following Hossenfelder
-    return rho_0*np.exp(-R/Rstar)*(1-(np.tanh(z/zstar))**2)
+    zstar = 0.196*Rstar**0.633 *u.kpc # following Hossenfelder
+    z*= u.kpc
+    ang = z/zstar * u.radian
+    return rho_0*np.exp(-R/Rstar)*(1-(np.tanh(ang))**2)
 
 def surface_density_disk(Rs, rho_0, Rstar):
     if hasattr(Rs, '__len__'):
@@ -143,27 +148,27 @@ def rho_disk_final(R):
 # plt.show()
 
 #-----------------------Final expressions------------------------------#
-G = 6.67e-11
-Mplanck = 2.18e-8
+G = c.G
+Mplanck = np.sqrt(c.hbar*c.c/c.G)
 
-vobs = example_df["Vobs"]
+vobs = example_df["Vobs"].values *u.km/u.s
 
 def rho(R, Q):
     return rho_gas(R) + 0.5*Q*rho_disk_final(R) + 0.7*Q*rho_buldge_final(R)
 
-def mass(R, Q):
-    return 4/3 * np.pi * R**3 * rho(R, Q)
+def mass(R, z, Q):
+    return 4/3 * np.pi * (R**2+z**2)**(3/2) * rho(R, Q)
 
-def v(Rs, Q):
+def v(Rs, z, Q):
     alpha = 5.7
     lamb = 0.05
     if hasattr(Rs, '__len__'):
-        return [np.sqrt(R*(G*mass(R, Q) + np.sqrt((alpha**3 * lamb**2/ Mplanck)* G * mass(R, Q)))) for R in Rs]
+        return [np.sqrt(R*(G*mass(R, z, Q) + np.sqrt((alpha**3 * lamb**2/ Mplanck)* G * mass(R, z, Q)))) for R in Rs]
     else:
-        return np.sqrt(Rs*(G*mass(Rs, Q) + np.sqrt((alpha**3 * lamb**2/ Mplanck)* G * mass(Rs, Q))))
+        return np.sqrt(Rs*(G*mass(Rs, z, Q) + np.sqrt((alpha**3 * lamb**2/ Mplanck)* G * mass(Rs, z, Q))))
 
-sopt, scov = curve_fit(v, rad, vobs, bounds=(1e-2, 15))
-print(sopt)
+sopt, scov = curve_fit(v, rad, vobs, bounds=(1e-2, 15)) #bounds from Hossenfelder on Q*
+
 
 def v_interpolated(R):
     return v(R, sopt[0])
